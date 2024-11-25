5fe3e1dbabefee06da3fc71113feea39e7f75ab8
Mathieu Lu <mathieu@symbiotic.coop>
2024-11-25 18:00:32 -0500

    Upgrade civix 24.09.1

--- primarycontact.php
+++ primarycontact.php
@@ -13,39 +13,12 @@ function primarycontact_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function primarycontact_civicrm_xmlMenu(&$files) {
-  _primarycontact_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function primarycontact_civicrm_install() {
   _primarycontact_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function primarycontact_civicrm_postInstall() {
-  _primarycontact_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function primarycontact_civicrm_uninstall() {
-  _primarycontact_civix_civicrm_uninstall();
 }
 
 /**
@@ -56,73 +29,6 @@ function primarycontact_civicrm_uninstall() {
 function primarycontact_civicrm_enable() {
   _primarycontact_civix_civicrm_enable();
 }
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function primarycontact_civicrm_disable() {
-  _primarycontact_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function primarycontact_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _primarycontact_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function primarycontact_civicrm_managed(&$entities) {
-  _primarycontact_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function primarycontact_civicrm_caseTypes(&$caseTypes) {
-  _primarycontact_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function primarycontact_civicrm_angularModules(&$angularModules) {
-  _primarycontact_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function primarycontact_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _primarycontact_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
 
 /**
  * Implements hook_civicrm_navigationMenu().
@@ -140,9 +46,6 @@ function primarycontact_civicrm_navigationMenu(&$menu) {
   ]);
   _primarycontact_civix_navigationMenu($menu);
 }
-
-
-
 
 /**
  * Implements hook_civicrm_post().
@@ -202,7 +105,6 @@ function primarycontact_civicrm_post($op, $objectName, $id, &$params) {
   }
 
 }
-
 
 /**
  * Implements hook_civicrm_postProcess().
@@ -265,7 +167,6 @@ function primarycontact_civicrm_postProcess($formName, &$form) {
 
 }
 
-
 /**
  * Implements hook_civicrm_tokens().
  *
@@ -286,7 +187,6 @@ function primarycontact_civicrm_tokens( &$tokens ) {
     );
   }
 }
-
 
 /**
  * Implements hook_civicrm_tokenValues().
